f75c3b5596ede536dc3245bb29a11ee99fc6e7e1
salimmia <46414940+salimmia@users.noreply.github.com>
2022-07-26 23:02:02 +0600

    Update EulerTour.cpp

--- Graph/EulerTour.cpp
+++ Graph/EulerTour.cpp
@@ -15,3 +15,20 @@ void dfs(int u, int p)
     }
     en[u] = timer;
 }
+
+/// Type 2:
+
+ll Linear[N], st[N], en[N], timer;
+
+void dfs(int u, int p)
+{
+    st[u] = ++timer;
+    Linear[timer] = u;
+
+    for (auto v : g[u]) {
+        if (v == p) continue;
+        dfs(v, u);
+    }
+    en[u] = ++timer;
+    Linear[timer] = u;
+}
