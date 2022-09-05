a9745e109cc65ac72bdd57c5a0ee17c27decb84c
salimmia <46414940+salimmia@users.noreply.github.com>
2022-09-06 05:53:18 +0600

    Update compress value.cpp

--- Tricks/compress value.cpp
+++ Tricks/compress value.cpp
@@ -1,15 +1,15 @@
-int compress() {
-	vector <int> v ; map<int, int> Map ;
-	for (int i = 1 ; i <= n ; i++) {
-		v.push_back(a[i]) ;
-	}
-	sort (v.begin(), v.end()) ;
-	v.erase(unique(v.begin(), v.end()), v.end()) ;
-	for (int i = 0 ; i < v.size() ; i++) {
-		Map[v[i]] = i + 1 ;
-	}
-	for (int i = 1 ; i <= n ; i++) {
-		a[i] = Map[a[i]] ;
-	}
-	return (int)v.size() ;
+void compress() {
+    vector<ll> v;
+
+    for (int i = 1; i <= n; i++)v.push_back(a[i]);
+
+    sort(v.begin(), v.end());
+
+    v.resize(unique(v.begin(), v.end()) - v.begin());
+
+    for (int i = 1; i <= n; i++) {
+        ll x = a[i];
+        x = lower_bound(v.begin(), v.end(), x) - v.begin() + 1;
+        a[i] = x;
+    }
 }
