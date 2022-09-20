a816a20201bf07e3013bbac09aa08ca85451ec69
salimmia <46414940+salimmia@users.noreply.github.com>
2022-09-20 16:33:50 +0600

    Update Marge Sort Tree.cpp

--- My/Data Structure/Marge Sort Tree.cpp
+++ My/Data Structure/Marge Sort Tree.cpp
@@ -12,3 +12,13 @@ void init(int node,int be,int en){
     init(node*2+1,mid+1,en);
     merge(Tree[node*2].begin(),Tree[node*2].end(),Tree[node*2+1].begin(),Tree[node*2+1].end(),back_inserter(Tree[node]));
 }
+
+int query(int node,int be,int en,int i,int j,int val){
+    if(be>j or en<i)return 0;
+    if(be>=i and en<=j){
+        int small=lower_bound(Tree[node].begin(),Tree[node].end(),val)-Tree[node].begin();
+        return small;
+    }
+    int mid=(be+en)/2;
+    return query(node*2,be,mid,i,j,val)+query(node*2+1,mid+1,en,i,j,val);
+}
