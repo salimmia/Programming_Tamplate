88dae0f965dd938e4091e49b8762f2b2f00f6d22
salimmia <46414940+salimmia@users.noreply.github.com>
2022-09-25 16:59:46 +0600

    Update Kmp.cpp

--- My/String/Kmp.cpp
+++ My/String/Kmp.cpp
@@ -6,5 +6,14 @@ vector<int> prefix_function (string s) {
         if (s[i] == s[j])  ++j;
         pi[i] = j;
     }
+//     for (int i = 0; i < n; i++) {
+//         for (char c = 'a'; c <= 'z'; c++) {
+//             if (c == s[i])dp[i][c - 'a'] = i + 1;
+//             else if (i > 0)dp[i][c - 'a'] = dp[pi[i - 1]][c - 'a'];
+
+//             cout << dp[i][c - 'a'] << " ";
+//         }
+//         cout << endl;
+//     }
     return pi;
 }
