93385650f6e77e78672e82869d06729025cbae2f
salimmia <46414940+salimmia@users.noreply.github.com>
2022-09-26 23:43:50 +0600

    Create bigmod.cpp

---
+++ My/Number Theory/bigmod.cpp
@@ -0,0 +1,51 @@
+
+ll inv[mx], invfact[mx], fact[mx];
+
+ll add(ll a, ll b)
+{
+    a += b;
+    if (a >= mod)a -= mod;
+    return a;
+}
+ll sub(ll a, ll b)
+{
+    a -= b;
+    if (a < 0)a += mod;
+    return a;
+}
+ll mul(ll a, ll b)
+{
+    ll re = a;
+    re *= b;
+    if (re >= mod)re %= mod;
+    return re;
+}
+
+ll bigmod(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = mul(res, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+
+void Fact()
+{
+    fact[0] = 1;
+    for (int i = 1; i < mx; i++) fact[i] = mul(fact[i - 1], i);
+
+    invfact[mx - 1] = bigmod(fact[mx - 1], mod - 2);
+
+    for (int i = mx - 2; i >= 0; i--) invfact[i] = mul(invfact[i + 1], i + 1);
+}
+
+void mod_inverse()
+{
+    inv[1] = 1;
+    for (int i = 2; i < mx; ++i) {
+        inv[i] = sub(mod, mul((mod / i), inv[mod % i]));
+    }
+}
